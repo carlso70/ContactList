83ab9c1b84fabd13a68aceb9d7dd801415f592db
Jimmy Carlson <carlso70@purdue.edu>
2016-07-22 15:02:52 -0500

    Setup intents to switch activities, and create button listener

--- app/src/main/java/com/example/panth/addressbook/AddContactActivity.java
+++ app/src/main/java/com/example/panth/addressbook/AddContactActivity.java
@@ -9,5 +9,8 @@ public class AddContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+
+        //TODO when done with activity don't forget to finish it
     }
 }
--- app/src/main/java/com/example/panth/addressbook/AddressBookActivity.java
+++ app/src/main/java/com/example/panth/addressbook/AddressBookActivity.java
@@ -1,9 +1,11 @@
 package com.example.panth.addressbook;
 
+import android.content.Intent;
 import android.location.Address;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -13,22 +15,32 @@ public class AddressBookActivity extends AppCompatActivity {
     private Button bAddContact;
     private Button bRemoveContact;
     private AddressBook addressBook;
+    public static final String ADDRESSBOOK_KEY = "addressBook";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address_book);
 
+        // TODO implement saving the address book
+        addressBook = new AddressBook();
+
         lvContacts = (ListView) findViewById(R.id.Contacts);
         bAddContact = (Button) findViewById(R.id.addContactButton);
         bRemoveContact = (Button) findViewById(R.id.removeContactButton);
 
-        // TODO implement saving the address book
-        addressBook = new AddressBook();
-
+        // Create contact button
+        bAddContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createContact();
+            }
+        });
     }
 
     private void createContact() {
-
+        Intent intent = new Intent(AddressBookActivity.this, AddContactActivity.class);
+        intent.putExtra(ADDRESSBOOK_KEY, addressBook);
+        startActivity(intent);
     }
 }
